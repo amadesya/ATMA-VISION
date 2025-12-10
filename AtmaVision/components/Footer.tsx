c058f7e60302ab43742348589391bfd0cadb7dac
amadesya <ultrashitband@gmail.com>
2025-12-11 00:39:19 +0300

    телеграмм ссылка

--- AtmaVision/components/Footer.tsx
+++ AtmaVision/components/Footer.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Instagram, Youtube } from 'lucide-react';
+import { Instagram, Youtube} from 'lucide-react';
 
 export const Footer: React.FC = () => {
   return (
@@ -56,11 +56,27 @@ export const Footer: React.FC = () => {
                >
                  <Youtube size={16} />
                </a>
+               <a
+  href="https://t.me/ioanna_dir"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="bg-blue-500 p-2 rounded-full text-white hover:bg-blue-600 transition-colors"
+  title="Telegram"
+>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+  >
+    <title>Telegram icon</title>
+    <path d="M23.91 3.79L20.3 20.84c-.25 1.21-.98 1.5-2 .94l-5.5-4.07-2.66 2.57c-.3.3-.55.56-1.1.56-.72 0-.6-.27-.84-.95L6.3 13.7l-5.45-1.7c-1.18-.35-1.19-1.16.26-1.75l21.26-8.2c.97-.43 1.9.24 1.53 1.73z"/>
+  </svg>
+</a>
+
             </div>
           </div>
-        </div>
-        <div className="mt-12 pt-8 border-t border-gray-100 text-center text-gray-400 text-sm">
-          &copy; 2025 ATMA VISION. Все права защищены.
         </div>
       </div>
     </footer>
